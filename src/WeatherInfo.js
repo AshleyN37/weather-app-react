fc6f831e358dd76cd6914808ce5224954faa1836
AshleyN37 <ashleymness8@gmail.com>
2021-02-19 11:21:26 -0500

    convert temperature from Fahrenheit to Celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,51 +1,42 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherData(props) {
     return (
-        <div className="WeatherData">
-
-            <h1 className="current-city">{props.data.city}</h1>
-
+    <div className="WeatherData">
+      <h1 className="current-city">{props.data.city}</h1>
+      <img src={props.data.weatherIcon} alt={props.data.description} className="WeatherIcon" />
       
-    <img
-            src={props.data.weatherIcon}
-            alt={props.data.description}
-            className="WeatherIcon"
-            />
-          <div className="CurrentTemperature">{Math.round(props.data.temperature)} <sup>
-            <a href="#" id="farenheit" className="active">
-                        °F 
-                </a> | 
-                <a href="#" id="celsius">
-                    °C
-                </a>
-            </sup>  </div>
-      <div className="row justify-content-center">
-        <div className="col-3">
-          <div className="TodaysWeather">
-          <div>
-            <FormattedDate date={props.data.date} />
-          </div>
-              <div className="WeatherDescription text-capitalize">
-      {props.data.description}
-    </div>
-          </div>
-        </div>
 
-        <div className="col-3 FeelsHumWind">
-          <div className="FeelsLike">
-      Feels Like: {Math.round(props.data.feelsLike)}°
-    </div>
-            <div className="Humidity">
-      Humidity: {props.data.humidity}%
-    </div>
-            <div className="Wind">
-      Wind: {Math.round(props.data.wind)} mph
-    </div>
-        </div>
-      </div>
-            </div>
+      <WeatherTemperature fahrenheit={props.data.temperature} />
+   
+            
+            
+            <div className="row justify-content-center">
+              <div className="col-3">
+                <div className="TodaysWeather">
+                  <div>
+                    <FormattedDate date={props.data.date} />
+                    </div>
+                    <div className="WeatherDescription text-capitalize">
+                      {props.data.description}
+                      </div>
+                      </div>
+                      </div>
+                      <div className="col-3 FeelsHumWind">
+                        <div className="FeelsLike">
+                          Feels Like: {Math.round(props.data.feelsLike)}°
+                          </div>
+                          <div className="Humidity">
+                            Humidity: {props.data.humidity}%
+                            </div>
+                            <div className="Wind">
+                              Wind: {Math.round(props.data.wind)} mph
+                              </div>
+                              </div>
+                              </div>
+                              </div>
     )
 }
