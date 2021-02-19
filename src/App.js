a62a25c8464e90183dca0a5dfd83b6c3f5a0aeb1
AshleyN37 <ashleymness8@gmail.com>
2021-02-19 08:39:18 -0500

    Added a state that displays weather data from api call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import SearchEngine from "./SearchEngine";
 import TodaysWeather from "./TodaysWeather";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,9 +7,7 @@ function App() {
   return (
     <div className="App">
       
-      <SearchEngine />
-      <h1 className="current-city">Washington D.C.</h1>
-      <TodaysWeather />
+      <TodaysWeather defaultCity="Washington, D.C."/>
       <p class="open-source">
             <a href="https://github.com/AshleyN37/weather-app-react" target="blank">Open-source code</a>, by Ashley Ness
     
--- src/TodaysWeather.js
+++ src/TodaysWeather.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./TodaysWeather.css"
-import Date from "./Date";
 
 
-export default function TodaysWeather() {
-  return (
-    <div>
-       <img
-            src="https://wikiclipart.com/wp-content/uploads/2017/02/Partly-cloudy-clipart-clipartfest.jpeg"
-            alt="partly cloudy"
+export default function TodaysWeather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].main,
+      date: "Monday 02:10",
+      feelsLike: response.data.main.feels_like,
+      humidity: response.data.main.humidity,
+      weatherIcon: "https://ssl.gstatic.com/onebox/weather/64/snow_s_cloudy.png"
+
+    });
+  }
+
+
+  if (weatherData.ready) {
+    return (
+    <div className="Form">
+      <form className="search-form">
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          autocomplete="off"
+          autofocus="on"
+          className="form-control"
+          id="search-input"
+        />
+
+        <input type="submit" value="Search" className="btn btn-primary" />
+        <input type="button" value="Current Location" class="btn btn-secondary" />
+      </form>
+      <h1 className="current-city">{weatherData.city}</h1>
+
+      
+    <img
+            src={weatherData.weatherIcon}
+            alt={weatherData.description}
             className="WeatherIcon"
           />
-          <div className="CurrentTemperature">37 <sup>
+          <div className="CurrentTemperature">{Math.round(weatherData.temperature)} <sup>
             <a href="#" id="farenheit" className="active">
                         °F 
                 </a> | 
@@ -22,25 +57,34 @@ export default function TodaysWeather() {
       <div className="row justify-content-center">
         <div className="col-3">
           <div className="TodaysWeather">
-            <Date />
-              <div className="WeatherDescription">
-      Clouds
+          <div>{weatherData.date}</div>
+              <div className="WeatherDescription text-capitalize">
+      {weatherData.description}
     </div>
           </div>
         </div>
 
         <div className="col-3 FeelsHumWind">
           <div className="FeelsLike">
-      Feels Like: 27°
+      Feels Like: {weatherData.feelsLike}°
     </div>
             <div className="Humidity">
-      Humidity: 41%
+      Humidity: {weatherData.humidity}%
     </div>
             <div className="Wind">
-      Wind: 8 mph
+      Wind: {weatherData.wind} mph
     </div>
         </div>
       </div>
-    </div>
+      </div>
   );
+
+  } else {
+    let apiKey = "5cb1aa65264597e34c41305199c5cf9e";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+
+    return "Loading"
+  }
+
 }
